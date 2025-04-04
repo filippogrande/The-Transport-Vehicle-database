9a232f06662d4218b9c3526f1fb84cd74dfaf136
Filippo Moscatelli <filipp28mo@gmail.com>
2025-04-04 08:49:31 +0200

    password change

--- Website/dbconnect.php
+++ Website/dbconnect.php
@@ -2,7 +2,7 @@
 $host = 'localhost';  // O l'indirizzo IP del server PostgreSQL
 $dbname = 'ttvd_db';  // Nome del database creato
 $user = 'ttvd_user';  // Nome utente del database
-$password = "d'KKukqavo";  // Password dell'utente del database
+$password = "7GMMg50Mhy";  // Password dell'utente del database
 
 // Connessione con PDO
 $dsn = "pgsql:host=$host;dbname=$dbname";
